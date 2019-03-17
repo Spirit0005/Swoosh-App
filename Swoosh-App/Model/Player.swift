c9f6b77db34452ef26caf8eaa576d41f703ca3ed
Hamza <m_hamzak1997@yahoo.com>
2019-03-17 23:30:19 +0500

    App done

---
+++ Swoosh-App/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Swoosh-App
+//
+//  Created by Hamza  on 17/03/2019.
+//  Copyright © 2019 Hamza . All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    
+    var gender:String?
+    var levle:String?
+}
